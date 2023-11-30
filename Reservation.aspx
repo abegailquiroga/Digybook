aa9003d4d7d68a4e16572693ff2034d1d5eea91d
AEIA <45816787+abegailquiroga@users.noreply.github.com>
2023-11-30 10:32:44 +0800

    Add files via upload

---
+++ Reservation.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Reservation.aspx.cs" Inherits="YEARBOOK.Reservation" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>MONITOR CONFLICT</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #f0f0f0; /* Light background color */
+        }
+
+        h1, h2 {
+            color: #800000; /* Maroon */
+        }
+
+        .form-group {
+            color: #000080; /* Navy */
+        }
+
+        .btn-primary {
+            background-color: #ffdb58; /* Mustard */
+            border-color: #ffdb58; /* Mustard */
+            color: #800000; /* Maroon */
+        }
+
+        .btn-primary:hover {
+            background-color: #ffd700; /* Gold */
+            border-color: #ffd700; /* Gold */
+            color: #800000; /* Maroon */
+        }
+
+        .table th, .table td {
+            border-color: #800000; /* Maroon */
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server" class="container mt-5">
+        <div>
+            <h1>Reservations</h1>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Id" CssClass="table table-striped">
+                <Columns>
+                    <asp:BoundField DataField="User" HeaderText="User" SortExpression="User" />
+                    <asp:BoundField DataField="StartTime" HeaderText="Start Time" SortExpression="StartTime" />
+                    <asp:BoundField DataField="EndTime" HeaderText="End Time" SortExpression="EndTime" />
+                </Columns>
+            </asp:GridView>
+
+            <div class="mt-4">
+                <h2>Make Reservation</h2>
+                <div class="form-group">
+                    <asp:TextBox ID="txtUser" runat="server" CssClass="form-control" placeholder="User" required></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:TextBox ID="txtStartTime" runat="server" CssClass="form-control" TextMode="DateTimeLocal" placeholder="Start Time" required></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:TextBox ID="txtEndTime" runat="server" CssClass="form-control" TextMode="DateTimeLocal" placeholder="End Time" required></asp:TextBox>
+                </div>
+                <asp:Button ID="btnSubmit" runat="server" CssClass="btn btn-primary" Text="Submit" OnClick="btnSubmit_Click" />
+            </div>
+        </div>
+    </form>
+
+    <!-- Bootstrap JS and Popper.js scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzYUEk/I9t2qaqDsKPCPm0Uk5c7Mz2YfoRSJo9g0vhj8S/6D
+    code>
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIElC4y4DIIiBl9SQQ8S+H/i+XIKqF"
+    code>
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIElC4y4DIIiBl9SQQ8S+H/i+XIKqF"
+    code>
+    </script>
+</body>
+</html>
---
+++ Reservation.aspx.cs
@@ -0,0 +1,106 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace YEARBOOK
+{
+    public partial class Reservation : Page
+    {
+
+        string connectionString = @"Server=localhost;Database=dbyearbook;Uid=root;Pwd=Abegail-1";
+
+        public int Id { get; set; }
+        public string User { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        private void BindGridView()
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var query = "SELECT Id, User, StartTime, EndTime FROM tblReservations";
+                using (var adapter = new MySqlDataAdapter(query, connection))
+                {
+                    var dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    GridView1.DataSource = dataTable;
+                    GridView1.DataBind();
+                }
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var user = txtUser.Text;
+            var startTime = DateTime.Parse(txtStartTime.Text);
+            var endTime = DateTime.Parse(txtEndTime.Text);
+
+            var newReservation = new Reservation { User = user, StartTime = startTime, EndTime = endTime };
+
+            if (!HasConflict(newReservation))
+            {
+                AddReservation(newReservation);
+                BindGridView();
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Conflict with existing reservations.');", true);
+            }
+        }
+
+        private bool HasConflict(Reservation newReservation)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var query = "SELECT Id, User, StartTime, EndTime FROM tblReservations WHERE StartTime < @EndTime AND EndTime > @StartTime";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@StartTime", newReservation.StartTime);
+                    command.Parameters.AddWithValue("@EndTime", newReservation.EndTime);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        return reader.HasRows;
+                    }
+                }
+            }
+        }
+
+        private void AddReservation(Reservation reservation)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var query = "INSERT INTO tblReservations (User, StartTime, EndTime) VALUES (@User, @StartTime, @EndTime)";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@User", reservation.User);
+                    command.Parameters.AddWithValue("@StartTime", reservation.StartTime);
+                    command.Parameters.AddWithValue("@EndTime", reservation.EndTime);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
